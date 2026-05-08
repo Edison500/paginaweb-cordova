a6f31edbeb0860a77749432c732f5ac22959a75a
edison <i202500803@cibertec.edu.pe>
2026-05-07 21:28:44 -0500

    mejoras our work y ajustes UI

--- .svelte-kit/generated/client/nodes/6.js
+++ .svelte-kit/generated/client/nodes/6.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/services/cabinet-painting/+page.svelte";
+export { default as component } from "../../../../src/routes/services/+page.svelte";
--- .svelte-kit/generated/client/nodes/7.js
+++ .svelte-kit/generated/client/nodes/7.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../../src/routes/services/cleaning/+page.svelte";
+export { default as component } from "../../../../src/routes/services/cabinet-painting/+page.svelte";
